b98eb3927d1f6d6be1c01910d3894f8b7efba8cb
Chris <chris@starforge.co.uk>
2011-02-17 14:35:44 +0000

    Removing redundant rollover code.

--- webui/templates/default/site.js
+++ webui/templates/default/site.js
@@ -1,11 +1,1 @@
 
-/* Add the rollover for the jump image that appears on most stages.
- */
-window.addEvent('domready', function() {
-    $$('img.pathimg').each(function(element, index) {
-        var src       = element.getProperty('src');
-        var extension = src.substring(src.lastIndexOf('.'),src.length);
-        element.addEvent('mouseenter', function() { element.setProperty('src', src.replace(extension, '_over' + extension)); });
-        element.addEvent('mouseleave', function() { element.setProperty('src', src); });
-    });
-});
