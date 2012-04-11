3c19a7f719dd8ef45644d736c72828c71066559a
Chris <chris@starforge.co.uk>
2012-04-11 15:15:31 +0100

    Appletargs is not a hashref!

--- course2wiki.pl
+++ course2wiki.pl
@@ -674,9 +674,9 @@ sub fix_applet {
     $logger -> print($logger -> WARNING,
                      "'".$course_xmltree -> {$themedir} -> {"theme"} -> {"title"}."' -> '".
                      $course_xmltree -> {$themedir} -> {"theme"} -> {"module"} -> {$moddir} -> {"title"}."' -> '".
-                     "$steptitle': detected applet tag showing '".($appletargs -> {"code"} || "Unknown")."'");
+                     "$steptitle': detected applet tag showing '".($appletargs{"code"} || "Unknown")."'");
 
-    return "<span class=\"error\">APPLET SHOULD GO HERE: code='".($appletargs -> {"code"} || "Unknown")."' width='".($appletargs -> {"width"} || "not set")."' height='".($appletargs -> {"height"} || "not set")."'</span>";
+    return "<span class=\"error\">APPLET SHOULD GO HERE: code='".($appletargs{"code"} || "Unknown")."' width='".($appletargs{"width"} || "not set")."' height='".($appletargs{"height"} || "not set")."'</span>";
 }
 
 
